9b65a07df66bab695d40c16f670673b1fb9d4331
Paul <llanquepol@gmail.com>
2023-05-26 23:40:53 -0500

    add rename file asyn

---
+++ day2/4-rename file/rename-asyn.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+// You have to pass the Relative path of the file from the current working directory
+fs.rename('data.txt', 'new_data.txt', (err) => {
+  if (err) throw err;
+
+  console.log(`File renamed successfuly`);
+});
+
+//To check it's Asynchronous nature !
+console.log('This method is Asynchronous');
